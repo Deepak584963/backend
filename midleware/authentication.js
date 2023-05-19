d0b7394e1391a8969fc65d68308488a4f6a55727
Deepak584953 <dk059511@gmail.com>
2023-05-19 17:50:33 +0530

    new

--- midleware/authentication.js
+++ midleware/authentication.js
@@ -1,23 +1,24 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const Deepak = require("../mongo");
 
-async function authentication(req , res, next) {
-    try {
-        const token = req.cookies.pika
-        console.log('authhhhh')
-   const verifytoken = jwt.verify(token,"shhhhhhhhhhhhhggttgggggggggg")
-//    console.log(`verifyed token ${verifytoken._id}`)
-   const user = await Deepak.findOne({_id:verifytoken._id, "tokens.token":token})
-//    if(!user){ throw new Error('user not found')}
-   req.user = user
-   req.id = user._id
-//    console.log(`dataaaaaaaaaaaaa ${user}`)
-   next()
-    } catch (error) {
-        res.status(401).send('token not verifyed')
-        console.log(error)
-    }
+async function authentication(req, res, next) {
+  try {
+    const token = req.cookies.pika;
 
-
+    const verifytoken = jwt.verify(token, "shhhhhhhhhhhhhggttgggggggggg");
+    //    console.log(`token ${verifytoken._id}`)
+    const user = await Deepak.findOne({
+      _id: verifytoken._id,
+      "tokens.token": token,
+    });
+    //    if(!user){ throw new Error('user not found')}
+    req.user = user;
+    req.id = user._id;
+    //    console.log(`dataaaaaaaaaaaaa ${user}`)
+    next();
+  } catch (error) {
+    res.status(401).send("token not verifyed");
+    console.log(error);
+  }
 }
-module.exports = authentication
+module.exports = authentication;
--- mongo.js
+++ mongo.js
@@ -1,52 +1,64 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-var jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
 
-mongoose.connect('mongodb://127.0.0.1:27017/deepak', { useUnifiedTopology: true, useNewUrlParser: true }).then(() => { console.log('connected') }).catch((err) => { console.log(err) })
+mongoose
+  .connect("mongodb://127.0.0.1:27017/deepak", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const playlistSchemaa = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    Email: String,
-    Password: String,
-    city: String,
-    state: String,
-    zip: String,
-    message: String,
-    tokens: [{
-        token: {
-            type:String,
-            // required:true
-        }
-    }]
+  firstName: String,
+  lastName: String,
+  Email: String,
+  Password: String,
+  city: String,
+  state: String,
+  zip: String,
+  message: String,
+  tokens: [
+    {
+      token: {
+        type: String,
+        // required:true
+      },
+    },
+  ],
 });
 
 playlistSchemaa.pre("save", async function (next) {
-    console.log(this.Password)
-    const dd = await bcrypt.hash(this.Password, 10)
-    console.log(dd)
-    this.Password = dd
-    next()
-})
+  console.log(this.Password);
+  const dd = await bcrypt.hash(this.Password, 10);
+  console.log(dd);
+  this.Password = dd;
+  next();
+});
 
 playlistSchemaa.methods.generateAuthToken = async function () {
-    
+  const token = jwt.sign({ _id: this._id }, "shhhhhhhhhhhhhggttgggggggggg");
+  this.tokens = this.tokens.concat({ token: token });
+  await this.save();
 
-    const token = jwt.sign({ _id: this._id }, 'shhhhhhhhhhhhhggttgggggggggg');
-    this.tokens = this.tokens.concat({ token: token });
-    await this.save()
-   
-    return token
-   
+  return token;
 };
 
-playlistSchemaa.methods.AddValue = async function (firstName, lastName, Email, message){
-    this.message = message;
-    
-    return message
-}
+playlistSchemaa.methods.AddValue = async function (
+  firstName,
+  lastName,
+  Email,
+  message
+) {
+  this.message = message;
 
+  return message;
+};
 
+const Deepak = mongoose.model("Deepak", playlistSchemaa);
 
-const Deepak = mongoose.model('Deepak', playlistSchemaa);
-
-module.exports = Deepak
+module.exports = Deepak;
